94854480f7042de55aea584186446b39d3741bb8
SM64Playground <sm64playground@gmail.com>
2024-08-30 18:43:35 -0500

    Add files via upload
    Signed-off-by: SM64Playground <sm64playground@gmail.com>

---
+++ bob/texture_include.inc.c
@@ -0,0 +1,125 @@
+Gfx bob_dl_custom_540C97C8_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_custom_540C97C8_c_rgba16[] = {
+	"levels/bob/custom_540C97C8_c.rgba16.png"
+};
+
+Gfx bob_dl_custom_6E4CCF7B_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_custom_6E4CCF7B_c_rgba16[] = {
+	"levels/bob/custom_6E4CCF7B_c.rgba16.png"
+};
+
+Gfx bob_dl_custom_5E8B4599_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_custom_5E8B4599_c_rgba16[] = {
+	"levels/bob/custom_5E8B4599_c.rgba16.png"
+};
+
+Gfx bob_dl_custom_6F3A95AB_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_custom_6F3A95AB_c_rgba16[] = {
+	"levels/bob/custom_6F3A95AB_c.rgba16.png"
+};
+
+Gfx bob_dl_custom_1EF080BB_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_custom_1EF080BB_c_rgba16[] = {
+	"levels/bob/custom_1EF080BB_c.rgba16.png"
+};
+
+Gfx bob_dl_custom_7175C630_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_custom_7175C630_c_rgba16[] = {
+	"levels/bob/custom_7175C630_c.rgba16.png"
+};
+
+Gfx bob_dl_custom_9522CE9_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_custom_9522CE9_c_rgba16[] = {
+	"levels/bob/custom_9522CE9_c.rgba16.png"
+};
+
+Gfx bob_dl_custom_7DBFA49F_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_custom_7DBFA49F_c_rgba16[] = {
+	"levels/bob/custom_7DBFA49F_c.rgba16.png"
+};
+
+Gfx bob_dl_custom_2808C46B_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_custom_2808C46B_c_rgba16[] = {
+	"levels/bob/custom_2808C46B_c.rgba16.png"
+};
+
+Gfx bob_dl_custom_7BEE76CF_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_custom_7BEE76CF_c_rgba16[] = {
+	"levels/bob/custom_7BEE76CF_c.rgba16.png"
+};
+
+Gfx bob_dl_custom_30E71B14_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_custom_30E71B14_c_rgba16[] = {
+	"levels/bob/custom_30E71B14_c.rgba16.png"
+};
+
+Gfx bob_dl_custom_32A41FC5_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_custom_32A41FC5_c_rgba16[] = {
+	"levels/bob/custom_32A41FC5_c.rgba16.png"
+};
+
+Gfx bob_dl_custom_41EBC9A1_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_custom_41EBC9A1_c_rgba16[] = {
+	"levels/bob/custom_41EBC9A1_c.rgba16.png"
+};
+
+Gfx bob_dl_custom_34CB6131_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_custom_34CB6131_c_rgba16[] = {
+	"levels/bob/custom_34CB6131_c.rgba16.png"
+};
+
+Gfx bob_dl_custom_5FAEE855_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_custom_5FAEE855_c_rgba16[] = {
+	"levels/bob/custom_5FAEE855_c.rgba16.png"
+};
+
+Gfx bob_dl_custom_5660AB2C_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_custom_5660AB2C_c_rgba16[] = {
+	"levels/bob/custom_5660AB2C_c.rgba16.png"
+};
+
+Gfx bob_dl_custom_190B252B_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_custom_190B252B_c_rgba16[] = {
+	"levels/bob/custom_190B252B_c.rgba16.png"
+};
+
+Gfx bob_dl_custom_46CA4F2F_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_custom_46CA4F2F_c_rgba16[] = {
+	"levels/bob/custom_46CA4F2F_c.rgba16.png"
+};
+
+Gfx bob_dl_custom_4F5F0DF3_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_custom_4F5F0DF3_c_rgba16[] = {
+	"levels/bob/custom_4F5F0DF3_c.rgba16.png"
+};
+
+Gfx bob_dl_custom_463FD8B5_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_custom_463FD8B5_c_rgba16[] = {
+	"levels/bob/custom_463FD8B5_c.rgba16.png"
+};
+
+Gfx bob_dl_custom_31FA4086_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_custom_31FA4086_c_rgba16[] = {
+	"levels/bob/custom_31FA4086_c.rgba16.png"
+};
+
+Gfx bob_dl_custom_605B2C64_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_custom_605B2C64_c_rgba16[] = {
+	"levels/bob/custom_605B2C64_c.rgba16.png"
+};
+
+Gfx bob_dl_custom_3DE597E8_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_custom_3DE597E8_c_rgba16[] = {
+	"levels/bob/custom_3DE597E8_c.rgba16.png"
+};
+
+Gfx bob_dl_custom_3B639E99_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_custom_3B639E99_c_rgba16[] = {
+	"levels/bob/custom_3B639E99_c.rgba16.png"
+};
+
+Gfx bob_dl_custom_6DAF90F6_c_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 bob_dl_custom_6DAF90F6_c_rgba16[] = {
+	"levels/bob/custom_6DAF90F6_c.rgba16.png"
+};
+
